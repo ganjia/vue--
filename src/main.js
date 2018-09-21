a154d150e0ddb055c29ce38afa4226df253dd9f3
ganjia <747206446@qq.com>
2018-09-21 15:25:01 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// 入口文件
+import Vue from 'vue'
+
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+import "./lib/mui/css/mui.min.css";
+// 导入APP 根组件
+import App from './App.vue'
+import router from './router'
+console.log('ok')
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router
+})
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//注册
+Vue.use(VueRouter)
+const router = new VueRouter({
+    routes: [
+        // { path: '/', component: DemoHome },
+        // { path: '/about', component: DemoAbout },
+        // { path: '/contact', component: DemoContact }
+    ]
+})
+export default router
